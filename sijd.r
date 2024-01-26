23a5fa3b98f0ad0ef993020f985eb2a666cd7014
levdalba <levanidalbashvili00@gmail.com>
2024-01-26 08:46:47 +0700

    Update sijd.r

--- sijd.r
+++ sijd.r
@@ -1,28 +1,33 @@
-library(ggplot2)
-library(dplyr)
-library(stringr)
-library(tidyr)
-library(forcats)
-
+# Assuming 'data' is your data frame
 # Set working directory
 setwd("C:\\Users\\levan\\OneDrive\\Documents\\Final project r\\stack-overflow-developer-survey-2023")
 
+# Read the dataset
 data <- read.csv("survey_results_public.csv")
 
-# Remove rows with NA values in the 'LanguageHaveWorkedWith' column
-data <- data[!is.na(data$LanguageHaveWorkedWith), ]
+# Convert 'YearsCode' and 'ConvertedCompYearly' to numeric
+data$YearsCode <- as.numeric(as.character(data$YearsCode))
+data$ConvertedCompYearly <- as.numeric(as.character(data$ConvertedCompYearly))
 
-# Count occurrences of each programming language
-lang_counts <- data %>%
-  mutate(LanguageHaveWorkedWith = str_split(LanguageHaveWorkedWith, ";")) %>%
-  unnest(c(LanguageHaveWorkedWith)) %>%
-  count(LanguageHaveWorkedWith) %>%
-  arrange(desc(n))
+# Remove rows with missing or non-numeric values in either column
+data <- data[!is.na(data$YearsCode) & !is.na(data$ConvertedCompYearly), ]
 
-# Create the bar plot
-ggplot(lang_counts, aes(x = n, y = fct_reorder(LanguageHaveWorkedWith, n))) +
-  geom_col(fill = 'lightcoral') +
-  scale_x_continuous(limits = c(0, 60000), breaks = seq(0, 60000, by = 5000)) +  # Set x-axis range and breaks
-  labs(title = 'Top Programming Languages Used by Developers',
-       x = 'Number of Developers',
-       y = 'Programming Language')
+# Remove rows with ConvertedCompYearly after 15000000
+data <- data[data$ConvertedCompYearly <= 1000000, ]
+
+# Create scatter plot
+scatter_plot <- ggplot(data, aes(x = YearsCode, y = ConvertedCompYearly, color = MainBranch, size = WorkExp)) +
+  geom_point(alpha = 0.7) +
+  labs(title = 'Scatter Plot of Salary vs. Years of Coding Experience',
+       x = 'Years of Coding Experience',
+       y = 'Converted Compensation Yearly') +
+  theme_minimal() +
+  scale_color_manual(values = c("#4C72B0", "#55A868", "#C44E52", "#8172B2", "#CCB974", "#64B5CD", "#EAEAF2")) +
+  scale_size_continuous(range = c(1, 5)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 1000000), breaks = seq(0, 1000000, by = 100000))
+
+print(scatter_plot)
+unique_currencies <- unique(data$Currency)
+
+# Print unique currencies
+print(unique_currencies)
